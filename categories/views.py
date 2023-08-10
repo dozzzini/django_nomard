ab5bcb7cca0363c2e8efe4dbdd05f72c79778860
dozzzini <dozzzini@naver.com>
2023-08-10 15:58:39 +0900

    before  serializer refactoring

--- categories/views.py
+++ categories/views.py
@@ -2,43 +2,46 @@ from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
 from .models import Category
 from .serializers import CategorySerializer
 
 
-@api_view(["GET", "POST"])
-def categories(request):
-    if request.method == "GET":
+class Categories(APIView):
+    # 원래대로 했으면 if ,elif가 연달아서 나올텐데 두 함수들을 보기 좋게(?)분리함.
+    def get(self, request):
         all_categories = Category.objects.all()
         serializer = CategorySerializer(all_categories, many=True)
-        # 모든 category를 가져다 JSON으로 바꾸는 과정
         return Response(serializer.data)
-    elif request.method == "POST":
-        # serializer을 가져가다 사용자가 보낸 데이터를 넘겨주는 과정
+
+    def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             new_category = serializer.save()
             return Response(
                 CategorySerializer(new_category).data,
             )
-            # serialzier로 데이터가 유효한지 확인
         else:
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def category(request, pk):
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        raise NotFound
+class CategoryDetail(APIView):
+    # API의 URL을 상세하게 작업(category 한 개, room 한 개처럼 하나만 작업) 하고 싶을 때
+    # get_object로 분리해서 get, put, delete method에서 공유될 수 있도록 작성
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound
+        return category
 
-    if request.method == "GET":
-        serializer = CategorySerializer(category)
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
         return Response(serializer.data)
-    elif request.method == "PUT":
+
+    def put(self, request, pk):
         serializer = CategorySerializer(
-            category,
+            self.get_object(pk),
             data=request.data,
             partial=True,
         )
@@ -47,6 +50,7 @@ def category(request, pk):
             return Response(CategorySerializer(updated_category).data)
         else:
             return Response(serializer.errors)
-    elif request.method == "DELETE":
-        category.delete()
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
         return Response(status=HTTP_204_NO_CONTENT)
